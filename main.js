5fa4ad7d07252f36a291ad260266a31a96bd4d73
mcs2515 <mcs2515@rit.edu>
2018-10-17 23:33:45 -0400

    fixed rect height, added horizontal dashed lines

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function rowConverter(row) {
   return {
 		stateInitials:row.state,
 		stateName:row.StateName,
-		totalprod: parseInt(row.totalprod /1000),
+		totalprod: row.totalprod,
 		year: row.year,
 		pesticides: parseInt(row.nAllNeonic),
   }
@@ -12,15 +12,14 @@ let stateMap = new Map();
 let state_lbl;
 let state_select;
 
-//var state_data = data.filter(function(d) {return d.stateInitials = 'AL'});
-
-
 let big_dataset, state_dataset;
 let xScale, yScale;
 let xAxis, yAxis;
 let xAxisGroup, yAxisGroup;
 let chart;
 let w, h;
+let leftMargin, rightMargin, bottomMargin;
+let tooltip;
 
 let key = (d) => d.app_name;
 
@@ -28,82 +27,115 @@ function makeChart(dataset) {
 	
 	w = 900;
 	h = 450;
+	
+	leftMargin = 70;
+	rightMargin = 20;
+	bottomMargin = 50;
    
 	// sort the data by downloads
   // uses built-in Array.sort() with comparator function
-  //dataset.sort((a,b) => b.pesticides - a.pesticides);
+  //dataset.sort((a,b) => b.totalprod - a.totalprod);
 
   chart = d3.select('#chart')
     .attr('width', w)
     .attr('height', h);
 	
-		//((graph width - 20) / length of  dataset) - padding
-	let barWidth = ((w - 20) / dataset.length) - 5; 
+	//((graph width - 20) / length of  dataset) - padding
+	let barWidth = ((w - rightMargin) / dataset.length) - 8; 
 
   // our range is limited from 0 to width - 100, 
   // which is for the 80 pixels on left for axis and 
   // 20 pixels on right for padding
   xScale = d3.scaleLinear()
-	//adding 1 more year to the max so last rect doesn't go off x-axis
+		//adding 1 more year to the max so last rect doesn't go off x-axis
     .domain([d3.min(dataset, (d) => d.year), Number(d3.max(dataset, (d) => d.year)) + 1])
-    .rangeRound([40, w - 20]);
+    .rangeRound([leftMargin, w - rightMargin]);
 
   yScale = d3.scaleLinear()
     .domain([0, d3.max(dataset, (d) => d.totalprod)])
-    .rangeRound([h-20, 20]);
+    .rangeRound([h-bottomMargin, 20]);
 
   // d3 allows scaling between colors
   let colorScale = d3.scaleLinear()
     .domain([0, d3.max(dataset, (d) => d.pesticides)])
     .range(['#fecf67', '#914420']);
 	
-	 console.log(d3.max(dataset, (d) => d.pesticides));
+		// Draw the horizontal grid lines
+  chart.append("g")			
+      .attr("class", "y-grid")
+			.style("stroke-dasharray",("3,3"))
+			.attr('transform', `translate(${leftMargin},0)`)
+      .call(
+					d3.axisLeft(yScale)
+          .tickSize(-w)
+          .tickFormat("")
+      );
 
-
+	//rects
   chart.selectAll('rect')
     .data(dataset, key)
     .enter()
     .append('rect')
     .attr('x', (d) => xScale(d.year))
-    .attr('y', (d) => h - 20 - yScale(d.totalprod))
+    .attr('y', (d) => yScale(d.totalprod))
     .attr('width', barWidth)
-    .attr('height', (d) => yScale(d.totalprod))
-		.attr('value', (d) => d.pesticides)
+    .attr('height', (d) => h - bottomMargin - yScale(d.totalprod))
+		.attr('value', (d) => d.totalprod)
     .attr('fill', (d) => colorScale(d.pesticides));
-
-  xAxis = d3.axisBottom(xScale);
-	xAxis.tickFormat(d3.format('d'))
-				.ticks(dataset.length);
 	
-  yAxis = d3.axisLeft(yScale)
 
-  // AXES
+	// AXES
+  xAxis = d3.axisBottom(xScale);
+	xAxis.tickFormat(d3.format('d')).ticks(dataset.length);
+	
+  yAxis = d3.axisLeft(yScale);
+	yAxis.tickFormat(d3.format(".2s"));
+  
   xAxisGroup = chart.append('g')
     .attr('class', 'axis-bottom')
-    .attr('transform', `translate(0, ${h - 20})`)
+    .attr('transform', `translate(0, ${h - bottomMargin})`)
     .call(xAxis);
 	
   yAxisGroup = chart.append('g')
     .attr('class', 'axis-left')
-    .attr('transform', `translate(40,0)`)
+    .attr('transform', `translate(70,0)`)
     .call(yAxis);
+	
+	// LABELS
+  chart.append("text")
+      .attr("transform", "rotate(-90)")
+			.attr("x", -(h-bottomMargin)/2)
+      .attr("y", 20)
+      .style("text-anchor", "middle")
+      .text("Total Production (lbs)");
+	
+	chart.append("text")
+		.attr("x", h)
+		.attr("y", (w/2))
+		.style("text-anchor", "middle")
+		.text("Years");
+}
+
+// gridlines in y axis function
+function createHorizontalLines() {		
+    return d3.axisLeft(yScale);
 }
 
 function updateChart(dataset) {
 
-	//let bars = chart.selectAll('rect').data(dataset, key);
+	let bars = chart.selectAll('rect').data(dataset, key);
 	
 	xScale.domain([d3.min(dataset, (d) => d.year), Number(d3.max(dataset, (d) => d.year)) + 1]);
 	yScale.domain([0, d3.max(dataset, (d) => d.totalprod)]);
 	
-	//bars.attr('height', (d) => yScale(d.totalprod));
-	
   // 4. select rects, rebind with key, use transition to then
   // update x,y, and height of bars
-//	bars
-//		.transition()
-//		.attr('x', (d) => xScale(d.year))
-//		.attr('y', (d) => h - 20 - yScale(d.totalprod));
+	bars
+		.transition()
+		.duration(500)
+		.attr('height', (d) => yScale(d.totalprod))
+		.attr('x', (d) => xScale(d.year))
+		.attr('y', (d) => h - bottomMargin - yScale(d.totalprod));
 	
   // After that, alway update xAxis scale, xAxisGroup with xAxis (call), and same for yAxis scale and yAxisGroup
 	xAxis.scale(xScale);
@@ -200,9 +232,10 @@ function createStateMap(){
 window.onload = function () {
 	
 	//set variables
-	let initials = "NY";
+	let initials = "AL";
 	state_select = d3.select('#stateSelect');
 	state_lbl = document.querySelector('#stateName');
+	tooltip = d3.select("body").append("div").attr('id', 'tooltip').style("opacity", 0);
 	
 	//get the csv and call appropriate functions
   d3.csv('HoneyNeonic.csv', rowConverter)
